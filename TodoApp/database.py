9ad1b053864153c1154514afb99dac65ed326d81
Bobibend <juhasz.imre@complexpress.hu>
2026-02-11 02:45:13 +0100

    Set Render database

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:zoliabuzi1@localhost/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not SQLALCHEMY_DATABASE_URL:
+    SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:zoliabuzi1@localhost/TodoApplicationDatabase'
+
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
